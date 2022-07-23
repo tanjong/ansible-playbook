982b37734b834928ffd2270461b2dcf9dedba851
Tanjong <tanjong.njie@outlook>
2022-07-23 01:51:49 -0500

    simple nginx server + ansible automation

--- infrastructure/vnet.tf
+++ infrastructure/vnet.tf
@@ -18,36 +18,33 @@ resource "azurerm_virtual_network" "devlab_vnet" {
   tags = local.common_tags
 }
 
-
-resource "azurerm_subnet" "app" {
-  name                 = var.app
-  resource_group_name  = azurerm_resource_group.devlab_general_network_rg.name
-  virtual_network_name = azurerm_virtual_network.devlab_vnet.name
-  address_prefixes     = var.address_prefixes
+resource "azurerm_route" "route" {
+  name                = "route1"
+  resource_group_name = azurerm_resource_group.devlab_general_network_rg.name
+  route_table_name    = azurerm_route_table.devlab_rt.name
+  address_prefix      = "10.0.0.0/16"
+  next_hop_type       = "VnetLocal"
 }
 
 resource "azurerm_route_table" "devlab_rt" {
-  name                          = var.devlab_rt
-  location                      = azurerm_resource_group.devlab_general_network_rg.location
-  resource_group_name           = azurerm_resource_group.devlab_general_network_rg.name
-  disable_bgp_route_propagation = var.disable_bgp_route_propagation
-
-  route {
-    name           = var.devlab_route1
-    address_prefix = var.address_prefix
-    next_hop_type  = var.next_hop_type
-  }
-
-  tags = local.common_tags
+  name                = var.devlab_rt
+  location            = azurerm_resource_group.devlab_general_network_rg.location
+  resource_group_name = azurerm_resource_group.devlab_general_network_rg.name
 }
 
+resource "azurerm_subnet" "application_subnet" {
+  name                 = var.application_subnet
+  resource_group_name  = azurerm_resource_group.devlab_general_network_rg.name
+  virtual_network_name = azurerm_virtual_network.devlab_vnet.name
+  address_prefixes     = var.address_prefixes_application
+}
 
-resource "azurerm_subnet_route_table_association" "devlab_rt_server_assoc" {
-  subnet_id      = azurerm_subnet.app.id
+resource "azurerm_subnet_route_table_association" "elitedev_rtb_assoc_application" {
+  subnet_id      = azurerm_subnet.application_subnet.id
   route_table_id = azurerm_route_table.devlab_rt.id
 }
 
-resource "azurerm_subnet_network_security_group_association" "devlab_nsg_app_assoc" {
-  subnet_id                 = azurerm_subnet.app.id
+resource "azurerm_subnet_network_security_group_association" "devlab_nsg_assoc_application_subnet" {
+  subnet_id                 = azurerm_subnet.application_subnet.id
   network_security_group_id = azurerm_network_security_group.devlab_nsg.id
 }
