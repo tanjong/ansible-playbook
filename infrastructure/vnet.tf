6b06947f37f301b263dc48b0e76e94e7606b3595
Tanjong <tanjong.njie@outlook>
2022-07-22 21:38:55 -0500

    simple nginx server + ansible automation

--- infrastructure/vnet.tf
+++ infrastructure/vnet.tf
@@ -15,6 +15,12 @@ resource "azurerm_virtual_network" "devlab_vnet" {
   resource_group_name = azurerm_resource_group.devlab_general_network_rg.name
   address_space       = var.address_space
 
+ subnet {
+    name           = "app_subnet"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.devlab_nsg.id
+  }
+
   tags = local.common_tags
 }
 
