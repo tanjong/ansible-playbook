abb6ac37e9f741542e1105cfd3857921199a018f
Tanjong <tanjong.njie@outlook>
2022-07-26 13:08:36 -0500

    simple nginx server + automation

--- infrastructure/vm.tf
+++ infrastructure/vm.tf
@@ -57,7 +57,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
